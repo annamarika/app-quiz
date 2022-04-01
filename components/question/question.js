fddc7e6d4d767b97a6472b2fa5f0af94cbd45acf
Anna Holtmann <anna_maria.heuer@mac.com>
2022-04-01 14:03:12 +0200

    add-more-js

---
+++ components/question/question.js
@@ -0,0 +1,26 @@
+export default function QuestionCard(questionButton) {
+
+   
+
+
+const answer = questionButton.querySelector('[data-js="question__answer__js"]');
+const showButton = questionButton.querySelector('[data-js="question__button__show__js"]');
+const hideButton = questionButton.querySelector('[data-js="question__button__hide__js"]');
+
+
+showButton.addEventListener("click", () => {
+    answer.classList.toggle("question__answer--hide");
+    hideButton.classList.toggle("question__button--hide");
+    showButton.classList.toggle("question__button--hide");
+  
+});
+
+hideButton.addEventListener("click", () => {
+    hideButton.classList.toggle("question__button--hide");
+    answer.classList.toggle("question__answer--hide"); 
+showButton.classList.toggle("question__button--hide");
+
+
+    
+});
+}
